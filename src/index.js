50d5438898d9ade6b2deac1e3e280752fa5dd2fb
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-24 19:53:21 +0200

    Import Redux Provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// React-redux
+import { Provider } from 'react-redux';
+import store from './reducers/configureStore';
+
 import App from './App';
-import './index.css';
+import Categories from './routes/Categories';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </Provider>
+    ,
   </React.StrictMode>,
-  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

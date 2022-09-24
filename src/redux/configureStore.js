134f2d967cbbee8f5a209c7de030e1513bfcadc7
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-24 10:42:23 +0200

    Configure eslint

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,7 +5,10 @@ import thunk from 'redux-thunk';
 import categoriesReducer from './categories/categories';
 import booksReducer from './books/books';
 
-
+const rootReducer = combineReducers({
+  categoriesReducer,
+  booksReducer,
+});
 const store = createStore(rootReducer,
   applyMiddleware(thunk)); // creating store from the root reducer
 export default store;
